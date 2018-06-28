63e286d4476b064c66f8b87132181201b7148b3e
gilemon <gilemon@MacBook-Pro.localdomain>
2018-06-28 18:03:18 +0200

    Search bar determine query type based on string length & prefix bytes

--- public/src/js/controllers/search.js
+++ public/src/js/controllers/search.js
@@ -17,48 +17,56 @@ angular.module('insight.search').controller('SearchController',
     $scope.q = '';
     $scope.loading = false;
   };
+  
+  $scope.defineSearchType = function(searchString) {
+    if (searchString.length == 64) {
+      var block_flag = false;
+      for (var i = 0; i < 2; i++) {
+        if (searchString[i] == '0') {
+          block_flag = true;
+        } else {
+          block_flag = false;
+          break;
+        }
+      }
+      if (block_flag == true) {
+        return [Block, 'blockHash', 'block/'];
+      } else {
+        return [Transaction, 'txId', 'tx/'];
+      }
+    } else if (searchString.length == 38 || searchString.length === 34) {
+      return [Address, 'addrStr', 'address/'];
+    }
+    if(isFinite(searchString)) {
+      return [BlockByHeight, 'blockHeight', 'block/'];
+    } else {
+      return false
+    }
+  }
 
   $scope.search = function() {
     var q = $scope.q;
+    var searchType = $scope.defineSearchType(q);
     $scope.badQuery = false;
     $scope.loading = true;
 
-    Block.get({
-      blockHash: q
-    }, function() {
-      _resetSearch();
-      $location.path('block/' + q);
-    }, function() { //block not found, search on TX
-      Transaction.get({
-        txId: q
-      }, function() {
+    if (searchType) {
+      var searchTypeObj = {};
+      searchTypeObj[searchType[1]] = q;
+      searchType[0].get(
+        searchTypeObj
+      , function() {
         _resetSearch();
-        $location.path('tx/' + q);
-      }, function() { //tx not found, search on Address
-        Address.get({
-          addrStr: q
-        }, function() {
-          _resetSearch();
-          $location.path('address/' + q);
-        }, function() { // block by height not found
-          if (isFinite(q)) { // ensure that q is a finite number. A logical height value.
-            BlockByHeight.get({
-              blockHeight: q
-            }, function(hash) {
-              _resetSearch();
-              $location.path('/block/' + hash.blockHash);
-            }, function() { //not found, fail :(
-              $scope.loading = false;
-              _badQuery();
-            });
-          }
-          else {
-            $scope.loading = false;
-            _badQuery();
-          }
-        });
+        $location.path(searchType[2] + q);
+      }, function() {
+          //not found, fail :(
+          $scope.loading = false;
+          _badQuery();
       });
-    });
+    } else {
+      $scope.loading = false;
+      _badQuery();
+    }
   };
 
 });
