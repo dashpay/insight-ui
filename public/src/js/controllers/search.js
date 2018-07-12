2f55ff7c8a12b30b3e9ce4287e4bbe9b9c48898a
gilemon <gilemon@MacBook-Pro.localdomain>
2018-07-12 21:32:08 +0200

    allow search by BlockHash with leading 0 and prepare faster search using new API endpoint

--- public/src/js/controllers/search.js
+++ public/src/js/controllers/search.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('insight.search').controller('SearchController',
-  function($scope, $routeParams, $location, $timeout, Global, Block, Transaction, Address, BlockByHeight) {
+  function($scope, $routeParams, $location, $timeout, Global, Block, Transaction, Address, BlockByHeight, Search) {
   $scope.global = Global;
   $scope.loading = false;
+  //to avoid eval for security reasons
+  $scope.factories = {Block: Block, Transaction: Transaction, Address: Address, BlockByHeight: BlockByHeight};
 
   var _badQuery = function() {
     $scope.badQuery = true;
@@ -18,55 +20,66 @@ angular.module('insight.search').controller('SearchController',
     $scope.loading = false;
   };
   
+  $scope.swap = function(theArray, indexA, indexB) {
+      var temp = theArray[indexA];
+      theArray[indexA] = theArray[indexB];
+      theArray[indexB] = temp;
+  };
+  
   $scope.defineSearchType = function(searchString) {
+    var searchArray = [
+      {factory: 'Block', object: 'blockHash', path: 'block/'}, 
+      {factory: 'Transaction', object:'txId', path: 'tx/'}, 
+      {factory: 'Address', object: 'addrStr', path: 'address/'}, 
+      {factory: 'BlockByHeight', object: 'blockHeight', path: 'block/'}
+    ];
     if (searchString.length == 64) {
-      var block_flag = false;
-      for (var i = 0; i < 2; i++) {
-        if (searchString[i] == '0') {
-          block_flag = true;
-        } else {
-          block_flag = false;
-          break;
-        }
-      }
-      if (block_flag == true) {
-        return [Block, 'blockHash', 'block/'];
-      } else {
-        return [Transaction, 'txId', 'tx/'];
+      if (!searchString.match(/^00/)) {
+        $scope.swap(searchArray, 0, 1);
       }
     } else if (searchString.length == 38 || searchString.length === 34) {
-      return [Address, 'addrStr', 'address/'];
+      $scope.swap(searchArray, 0, 2);
     }
-    if(isFinite(searchString)) {
-      return [BlockByHeight, 'blockHeight', 'block/'];
-    } else {
-      return false
+    if (isFinite(searchString)) {
+      $scope.swap(searchArray, 0, 3);
     }
+    return searchArray
   }
-
-  $scope.search = function() {
-    var q = $scope.q;
-    var searchType = $scope.defineSearchType(q);
-    $scope.badQuery = false;
-    $scope.loading = true;
-
-    if (searchType) {
+  
+  $scope.traverseSearch = function(searchTypeArray, q) {
+    if (searchTypeArray.length) {
       var searchTypeObj = {};
-      searchTypeObj[searchType[1]] = q;
-      searchType[0].get(
+      searchTypeObj[searchTypeArray[0].object] = q;
+      var path = searchTypeArray[0].path;
+      $scope.factories[searchTypeArray[0].factory].get(
         searchTypeObj
       , function() {
+        $scope.searchFound = true;
         _resetSearch();
-        $location.path(searchType[2] + q);
+        $location.path(path + q);
       }, function() {
-          //not found, fail :(
-          $scope.loading = false;
-          _badQuery();
+        $scope.traverseSearch(searchTypeArray.slice(1), q);
       });
     } else {
       $scope.loading = false;
       _badQuery();
     }
+  }
+
+  $scope.search = function() {
+    var q = $scope.q;
+    $scope.badQuery = false;
+    $scope.loading = true;
+
+    Search.get({
+      searchstr: q
+    }, function(res) {
+      $scope.traverseSearch(res.data, q);
+    }, function() { 
+      //not found, allow backward compatibilty if search route not available on API 
+      var searchTypeArray = $scope.defineSearchType(q);
+      $scope.traverseSearch(searchTypeArray, q);
+    });
   };
 
 });
