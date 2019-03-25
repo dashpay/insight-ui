90311b96ff8bf94b13e0f794be512dadd628e2f2
Ivan Shumkov <ivan@shumkov.ru>
2019-03-25 17:45:28 +0300

    Configure Travis build

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -xe
+
+# Ensure the tag matches the one in package.json, otherwise abort.
+PACKAGE_TAG=v"$(jq -r .version package.json)"
+if [[ "$PACKAGE_TAG" != "$TRAVIS_TAG" ]]; then
+  echo "Travis tag (\"$TRAVIS_TAG\") is not equal to package.json tag (\"$PACKAGE_TAG\"). Please push a correct tag and try again."
+  exit 1
+fi
+
+# Publish the npm module
+npm publish
+
+IMAGE_NAME="dashpay/insight"
+
+MAJOR_VERSION="$( cut -d '.' -f 1 <<< "$PACKAGE_TAG" )"
+
+# Build Docker image
+docker build -t "${IMAGE_NAME}:latest" \
+             -t "${IMAGE_NAME}:${PACKAGE_TAG}" \
+             --build-arg "MAJOR_VERSION=${MAJOR_VERSION}" \
+             --build-arg "VERSION=${PACKAGE_TAG}" \
+             .
+
+# Push images to the registry
+docker push "${IMAGE_NAME}:latest"
+docker push "${IMAGE_NAME}:${PACKAGE_TAG}"
