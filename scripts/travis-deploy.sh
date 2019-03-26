d0311faf91cfa98a40ad02b64feb61d27edddea9
Ivan Shumkov <ivan@shumkov.ru>
2019-03-26 19:17:24 +0300

    Build for last LTE version too

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -2,6 +2,18 @@
 
 set -xe
 
+# Update this whenever the latest Node.js LTS version changes (~ every year).
+# Do not forget to add this version to .travis.yml config also.
+LATEST_LTS_VERSION="10"
+
+# We want this command to succeed whether or not the Node.js version is the
+# latest (so that the build does not show as failed), but _only_ the latest
+# should be used to publish the module.
+if [ "$TRAVIS_NODE_VERSION" != "$LATEST_LTS_VERSION" ]; then
+  echo "Node.js v$TRAVIS_NODE_VERSION is not latest LTS version -- will not deploy with this version."
+  exit 0
+fi
+
 # Ensure the tag matches the one in package.json, otherwise abort.
 PACKAGE_TAG=v"$(jq -r .version package.json)"
 if [[ "$PACKAGE_TAG" != "$TRAVIS_TAG" ]]; then
