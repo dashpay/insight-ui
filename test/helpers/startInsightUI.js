1c199f7f0b2847ac860f1d511f819706af1a2ddd
abaranouski <andrei@paybear.io>
2019-06-26 16:23:20 +0300

    add UI tests

---
+++ test/helpers/startInsightUI.js
@@ -0,0 +1,113 @@
+'use strict';
+/* jshint ignore:start */
+const os = require('os');
+const { merge } = require('lodash');
+
+const startDashCore = require('@dashevo/dp-services-ctl/lib/services/dashCore/startDashCore');
+const startInsightUi = require('./container/startInsightUi');
+
+async function remove(services) {
+  const insightDeps = [
+    services.dashCore,
+  ];
+  await Promise.all(insightDeps.map(instance => instance.remove()));
+}
+
+/**
+ * @typedef InsightUI
+ * @property {DapiCore} dapiCore
+ * @property {DockerService} sync
+ * @property {Promise<void>} clean
+ * @property {Promise<void>} remove
+ */
+
+/**
+ * Create Insight UI instance
+ *
+ * @param {object} [options]
+ * @returns {Promise<InsightUI>}
+ */
+async function startInsightUI(options) {
+  const instances = await startInsightUi.many(1, options);
+  return instances[0];
+}
+
+/**
+ * Create Insight UI instances
+ *
+ * @param {Number} number
+ * @param {object} [options]
+ * @returns {Promise<InsightUI[]>}
+ */
+startInsightUI.many = async function many(number, options = {}) {
+  if (number < 1) {
+    throw new Error('Invalid number of instances');
+  }
+  if (number > 1) {
+    throw new Error("We don't support more than 1 instance");
+  }
+
+
+  const dashCoreInstancesPromise = startDashCore.many(number, options.dashCore);
+  const [dashCoreInstances] = await Promise.all([
+    dashCoreInstancesPromise,
+  ]);
+
+  const instances = [];
+
+  for (let i = 0; i < number; i++) {
+
+    const dashCore = dashCoreInstances[i];
+
+
+    const insightUIOptions = {
+      container: {},
+      config: {},
+      ...options.insightUI,
+    };
+
+    merge(insightUIOptions.config, {
+      servicesConfig: {
+        dashd: {
+          connect: [{
+            rpchost: `${dashCore.getIp()}`,
+            rpcport: `${dashCore.options.getRpcPort()}`,
+            rpcuser: `${dashCore.options.getRpcUser()}`,
+            rpcpassword: `${dashCore.options.getRpcPassword()}`,
+            zmqpubrawtx: `tcp://host.docker.internal:${dashCore.options.getZmqPorts().rawtx}`,
+            zmqpubhashblock: `tcp://host.docker.internal:${dashCore.options.getZmqPorts().hashblock}`,
+          }],
+        },
+      },
+    });
+
+
+    const insightUIPromise = await startInsightUI(insightUIOptions);
+
+    const [insightUi] = await Promise.all([
+      insightUIPromise,
+    ]);
+
+
+    const instance = {
+      insightUi,
+      dashCore,
+      async clean() {
+        await remove(instance);
+
+        const newServices = await startInsightUI(options);
+
+        Object.assign(instance, newServices);
+      },
+      async remove() {
+        await remove(instance);
+      },
+    };
+
+    instances.push(instance);
+  }
+
+  return instances;
+};
+
+module.exports = startInsightUI;
