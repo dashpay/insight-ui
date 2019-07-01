5e357571be9f01c480bb5bdfce9adfd1d5c2375d
Ivan Shumkov <ivan@shumkov.ru>
2019-07-01 17:01:36 +0300

    Configure jshint and introduce eslint for the new code

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,17 +1,17 @@
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    jasmineNodeOpts: {
-        defaultTimeoutInterval: 250000
-    },
-    specs: ['test.js'],
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 250000,
+  },
+  specs: ['test.js'],
 
-    multiCapabilities: [
-        {
-            browserName: 'firefox',
-        },
-        {
-            browserName: 'chrome'
-        }
-    ],
+  multiCapabilities: [
+    {
+      browserName: 'firefox',
+    },
+    {
+      browserName: 'chrome',
+    },
+  ],
 };
--- test/e2e/test.js
+++ test/e2e/test.js
@@ -1,6 +1,4 @@
-'use strict';
-/* jshint ignore:start */
-const {expect} = require('chai');
+const { expect } = require('chai');
 const startInsightUI = require('../../lib/test/startInsightUI');
 const wait = require('../../lib/test/util/wait');
 
@@ -13,219 +11,216 @@ const InsightUIOptions = require('../../lib/test/service-ctl/InsightUIOptions');
 let originalTimeout;
 
 describe('basic UI tests', () => {
-    let masterNode;
+  let masterNode;
 
-    let url;
-    let blockHash;
-    let trxHash;
+  let url;
+  let blockHash;
+  let trxHash;
 
-    beforeAll(async () => {
-        const rootPath = process.cwd();
+  beforeAll(async () => {
+    const rootPath = process.cwd();
 
-        const insightUIContainerOptions = {
-            volumes: [
-                `${rootPath}/dashcore-node:/insight/node_modules/@dashevo/insight-ui/dashcore-node`,
-                `${rootPath}/po:/insight/node_modules/@dashevo/insight-ui/po`,
-                `${rootPath}/public:/insight/node_modules/@dashevo/insight-ui/public`,
-            ],
-        };
+    const insightUIContainerOptions = {
+      volumes: [
+        `${rootPath}/dashcore-node:/insight/node_modules/@dashevo/insight-ui/dashcore-node`,
+        `${rootPath}/po:/insight/node_modules/@dashevo/insight-ui/po`,
+        `${rootPath}/public:/insight/node_modules/@dashevo/insight-ui/public`,
+      ],
+    };
 
-        const insighUIOptions = {
-            cacheNodeModules: true,
-            localAppPath: rootPath,
-            container: insightUIContainerOptions,
-        };
+    const insighUIOptions = {
+      cacheNodeModules: true,
+      localAppPath: rootPath,
+      container: insightUIContainerOptions,
+    };
 
-        InsightUIOptions.setDefaultCustomOptions(insighUIOptions);
+    InsightUIOptions.setDefaultCustomOptions(insighUIOptions);
 
 
-        [masterNode] = await startInsightUI.many(1);
+    [masterNode] = await startInsightUI.many(1);
 
-        url = `http://127.0.0.1:${masterNode.insightUi.options.getUiPort()}/insight/`;
+    url = `http://127.0.0.1:${masterNode.insightUi.options.getUiPort()}/insight/`;
 
-        await masterNode.dashCore.getApi().generate(15);
+    await masterNode.dashCore.getApi().generate(15);
+  });
 
+  afterAll(async () => {
+    const instances = [
+      masterNode,
+    ];
+
+    await Promise.all(instances.filter(i => i)
+      .map(i => i.remove()));
+  });
+
+  beforeEach(() => {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 250000;
+  });
+
+  afterEach(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+
+  describe('Home Page', () => {
+    it('should be able to open main page', async () => {
+      await browser.get(url);
+      const title = await browser.getTitle();
+      expect(title).equal('Home | Insight');
     });
 
-    afterAll(async () => {
-        const instances = [
-            masterNode,
-        ];
-
-        await Promise.all(instances.filter(i => i)
-            .map(i => i.remove()));
-
+    it('should be able to open block page', async () => {
+      await topPanel.openBlockPage();
+      const title = await browser.getTitle();
+      expect(title).equal('Home | Insight');
     });
 
-    beforeEach(function () {
-        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 250000;
+    it('should be able to open status page', async () => {
+      await topPanel.openStatusPage();
+      await wait(10000); // time to complete sync
+      const title = await browser.getTitle();
+      expect(title).equal('Status | Insight');
+
+      const syncProgress = await statusPage.getSyncProgress();
+      expect(syncProgress).equal('100% Complete');
+
+      const currentSyncStatus = await statusPage.getCurrentSyncStatus();
+      expect(currentSyncStatus).equal('finished');
+
+      const startDate = await statusPage.getStartDate();
+      expect(startDate).equal('Invalid date');
+
+      const initialBlockChainHeight = await statusPage.getInitialBlockChainHeight();
+      expect(Number.isInteger(parseInt(initialBlockChainHeight))).equal(true);
+
+      const syncedBlocks = await statusPage.getSyncedBlocks();
+      expect(syncedBlocks).equal('');
+
+      const skippedBlocks = await statusPage.getSkippedBlocks();
+      expect(skippedBlocks).equal('');
+
+      const syncType = await statusPage.getSyncType();
+      expect(syncType).equal('bitcore node');
+
+      const lastBlockHash = await statusPage.getLastBlockHash();
+      expect(lastBlockHash).not.equal(undefined);
+
+      const currentBlockchainTip = await statusPage.getCurrentBlockchainTip();
+      expect(currentBlockchainTip).not.equal(undefined);
+
+      const version = await statusPage.getVersion();
+      expect(Number.isInteger(parseInt(version))).equal(true);
+
+      const protocolVersion = await statusPage.getProtocolVersion();
+      expect(Number.isInteger(parseInt(protocolVersion))).equal(true);
+
+      const blocks = await statusPage.getBlocks();
+      expect(blocks).equal('15');
+
+      const timeOffset = await statusPage.getTimeOffset();
+      expect(timeOffset).equal('0');
+
+      const connections = await statusPage.getConnections();
+      expect(connections).equal('0');
+
+      const miningDifficulty = await statusPage.getMiningDifficulty();
+      expect(miningDifficulty).not.equal('');
+
+      const network = await statusPage.getNetwork();
+      expect(network).equal('testnet');
+
+      const proxySetting = await statusPage.getProxySetting();
+      expect(proxySetting).equal('');
+
+      const infoErrors = await statusPage.getInfoErrors();
+      expect(infoErrors).equal('');
     });
 
-    afterEach(function () {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    it('should be able search by block number', async () => {
+      const blockIdToSearch = '12';
+
+      topPanel.search(blockIdToSearch);
+      const currentUrl = await browser.getCurrentUrl();
+      expect(currentUrl).equal(`${url}block/${blockIdToSearch}`);
+
+      const blockId = (await blockPage.getBlockId()).replace('Block #', '');
+      expect(blockId).equal(blockIdToSearch);
+      blockHash = await blockPage.getBlockHash();
+
+      const numberOfTrxs = await blockPage.getNumberOfTrxs();
+      expect(numberOfTrxs).equal('1');
+
+      const height = await blockPage.getHeight();
+      expect(height).equal(`${blockIdToSearch} (Mainchain)`);
+
+      const blockReward = await blockPage.getBlockReward();
+      expect(blockReward).equal('500 DASH');
+
+      const timestamp = await blockPage.getTimestamp();
+      expect(timestamp).not.equal('');
+      const minedBy = await blockPage.getMinedBy();
+      expect(minedBy).equal('');
+      const merkleRoot = await blockPage.getMerkleRoot();
+      expect(merkleRoot).not.equal('');
+      const previousBlock = await blockPage.getPreviousBlock();
+      expect(previousBlock).equal(`${parseInt(blockId) - 1}`);
+      const difficulty = await blockPage.getDifficulty();
+      expect(difficulty).not.equal('');
+      const bits = await blockPage.getBits();
+      expect(bits).not.equal('');
+      const size = await blockPage.getSize();
+      expect(size).not.equal('');
+      const version = await blockPage.getVersion();
+      expect(Number.isInteger(parseInt(version))).equal(true);
+      const nonce = await blockPage.getNonce();
+      expect(Number.isInteger(parseInt(nonce))).equal(true);
+      const nextBlock = await blockPage.getNextBlock();
+
+      expect(nextBlock).equal(`${parseInt(blockId) + 1}`);
+      trxHash = await blockPage.getTrxHash();
     });
 
-    describe('Home Page', () => {
-        it('should be able to open main page', async () => {
-            await browser.get(url);
-            const title = await browser.getTitle();
-            expect(title).equal('Home | Insight');
-        });
+    it('should be able search by block hash', async () => {
+      const blockIdToSearch = '12';
+      topPanel.search(blockHash);
+      const currentUrl = await browser.getCurrentUrl();
+      expect(currentUrl).equal(`${url}block/${blockHash}`);
 
-        it('should be able to open block page', async () => {
-            await topPanel.openBlockPage();
-            const title = await browser.getTitle();
-            expect(title).equal('Home | Insight');
-        });
+      const blockId = (await blockPage.getBlockId()).replace('Block #', '');
+      expect(blockId).equal(blockIdToSearch);
+      expect(await blockPage.getBlockHash()).equal(blockHash);
 
-        it('should be able to open status page', async () => {
-            await topPanel.openStatusPage();
-            await wait(10000); // time to complete sync
-            const title = await browser.getTitle();
-            expect(title).equal('Status | Insight');
+      const numberOfTrxs = await blockPage.getNumberOfTrxs();
+      expect(numberOfTrxs).equal('1');
 
-            const syncProgress = await statusPage.getSyncProgress();
-            expect(syncProgress).equal("100% Complete");
+      const height = await blockPage.getHeight();
+      expect(height).equal(`${blockIdToSearch} (Mainchain)`);
 
-            const currentSyncStatus = await statusPage.getCurrentSyncStatus();
-            expect(currentSyncStatus).equal("finished");
+      const blockReward = await blockPage.getBlockReward();
+      expect(blockReward).equal('500 DASH');
 
-            const startDate = await statusPage.getStartDate();
-            expect(startDate).equal("Invalid date");
+      const timestamp = await blockPage.getTimestamp();
+      expect(timestamp).not.equal('');
+      const minedBy = await blockPage.getMinedBy();
+      expect(minedBy).equal('');
+      const merkleRoot = await blockPage.getMerkleRoot();
+      expect(merkleRoot).not.equal('');
+      const previousBlock = await blockPage.getPreviousBlock();
+      expect(previousBlock).equal(`${parseInt(blockId) - 1}`);
+      const difficulty = await blockPage.getDifficulty();
+      expect(difficulty).not.equal('');
+      const bits = await blockPage.getBits();
+      expect(bits).not.equal('');
+      const size = await blockPage.getSize();
+      expect(size).not.equal('');
+      const version = await blockPage.getVersion();
+      expect(Number.isInteger(parseInt(version))).equal(true);
+      const nonce = await blockPage.getNonce();
+      expect(Number.isInteger(parseInt(nonce))).equal(true);
+      const nextBlock = await blockPage.getNextBlock();
+      expect(nextBlock).equal(`${parseInt(blockId) + 1}`);
 
-            const initialBlockChainHeight = await statusPage.getInitialBlockChainHeight();
-            expect(Number.isInteger(parseInt(initialBlockChainHeight))).equal(true);
-
-            const syncedBlocks = await statusPage.getSyncedBlocks();
-            expect(syncedBlocks).equal("");
-
-            const skippedBlocks = await statusPage.getSkippedBlocks();
-            expect(skippedBlocks).equal("");
-
-            const syncType = await statusPage.getSyncType();
-            expect(syncType).equal("bitcore node");
-
-            const lastBlockHash = await statusPage.getLastBlockHash();
-            expect(lastBlockHash).not.equal(undefined);
-
-            const currentBlockchainTip = await statusPage.getCurrentBlockchainTip();
-            expect(currentBlockchainTip).not.equal(undefined);
-
-            const version = await statusPage.getVersion();
-            expect(Number.isInteger(parseInt(version))).equal(true);
-
-            const protocolVersion = await statusPage.getProtocolVersion();
-            expect(Number.isInteger(parseInt(protocolVersion))).equal(true);
-
-            const blocks = await statusPage.getBlocks();
-            expect(blocks).equal("15");
-
-            const timeOffset = await statusPage.getTimeOffset();
-            expect(timeOffset).equal("0");
-
-            const connections = await statusPage.getConnections();
-            expect(connections).equal("0");
-
-            const miningDifficulty = await statusPage.getMiningDifficulty();
-            expect(miningDifficulty).not.equal("");
-
-            const network = await statusPage.getNetwork();
-            expect(network).equal('testnet');
-
-            const proxySetting = await statusPage.getProxySetting();
-            expect(proxySetting).equal("");
-
-            const infoErrors = await statusPage.getInfoErrors();
-            expect(infoErrors).equal("");
-        });
-
-        it('should be able search by block number', async () => {
-            const blockIdToSearch = '12';
-
-            topPanel.search(blockIdToSearch);
-            const currentUrl = await browser.getCurrentUrl();
-            expect(currentUrl).equal(`${url}block/${blockIdToSearch}`);
-
-            const blockId = (await blockPage.getBlockId()).replace('Block #', '');
-            expect(blockId).equal(blockIdToSearch);
-            blockHash = await blockPage.getBlockHash();
-
-            const numberOfTrxs = await blockPage.getNumberOfTrxs();
-            expect(numberOfTrxs).equal("1");
-
-            const height = await blockPage.getHeight();
-            expect(height).equal(`${blockIdToSearch} (Mainchain)`);
-
-            const blockReward = await blockPage.getBlockReward();
-            expect(blockReward).equal('500 DASH');
-
-            const timestamp = await blockPage.getTimestamp();
-            expect(timestamp).not.equal("");
-            const minedBy = await blockPage.getMinedBy();
-            expect(minedBy).equal("");
-            const merkleRoot = await blockPage.getMerkleRoot();
-            expect(merkleRoot).not.equal("");
-            const previousBlock = await blockPage.getPreviousBlock();
-            expect(previousBlock).equal((parseInt(blockId) - 1) + '');
-            const difficulty = await blockPage.getDifficulty();
-            expect(difficulty).not.equal("");
-            const bits = await blockPage.getBits();
-            expect(bits).not.equal("");
-            const size = await blockPage.getSize();
-            expect(size).not.equal("");
-            const version = await blockPage.getVersion();
-            expect(Number.isInteger(parseInt(version))).equal(true);
-            const nonce = await blockPage.getNonce();
-            expect(Number.isInteger(parseInt(nonce))).equal(true);
-            const nextBlock = await blockPage.getNextBlock();
-
-            expect(nextBlock).equal((parseInt(blockId) + 1) + '');
-            trxHash = await blockPage.getTrxHash();
-        });
-
-        it('should be able search by block hash', async () => {
-            const blockIdToSearch = "12";
-            topPanel.search(blockHash);
-            const currentUrl = await browser.getCurrentUrl();
-            expect(currentUrl).equal(`${url}block/${blockHash}`);
-
-            const blockId = (await blockPage.getBlockId()).replace('Block #', '');
-            expect(blockId).equal(blockIdToSearch);
-            expect(await blockPage.getBlockHash()).equal(blockHash);
-
-            const numberOfTrxs = await blockPage.getNumberOfTrxs();
-            expect(numberOfTrxs).equal("1");
-
-            const height = await blockPage.getHeight();
-            expect(height).equal(`${blockIdToSearch} (Mainchain)`);
-
-            const blockReward = await blockPage.getBlockReward();
-            expect(blockReward).equal('500 DASH');
-
-            const timestamp = await blockPage.getTimestamp();
-            expect(timestamp).not.equal("");
-            const minedBy = await blockPage.getMinedBy();
-            expect(minedBy).equal("");
-            const merkleRoot = await blockPage.getMerkleRoot();
-            expect(merkleRoot).not.equal("");
-            const previousBlock = await blockPage.getPreviousBlock();
-            expect(previousBlock).equal((parseInt(blockId) - 1) + '');
-            const difficulty = await blockPage.getDifficulty();
-            expect(difficulty).not.equal("");
-            const bits = await blockPage.getBits();
-            expect(bits).not.equal("");
-            const size = await blockPage.getSize();
-            expect(size).not.equal("");
-            const version = await blockPage.getVersion();
-            expect(Number.isInteger(parseInt(version))).equal(true);
-            const nonce = await blockPage.getNonce();
-            expect(Number.isInteger(parseInt(nonce))).equal(true);
-            const nextBlock = await blockPage.getNextBlock();
-            expect(nextBlock).equal((parseInt(blockId) + 1) + '');
-
-            expect(await blockPage.getTrxHash()).equal(trxHash);
-        });
+      expect(await blockPage.getTrxHash()).equal(trxHash);
     });
-
+  });
 });
